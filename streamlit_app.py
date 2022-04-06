bb6cab8e54948acf28d973efa93c10b25e63d29c
Guilherme Húngaro <89612743+GuiHungaro@users.noreply.github.com>
2022-04-06 19:30:33 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ predictions = pcc.predict_model(best, data=df, raw_score=True)
 
 
 #Criando função para plotar os gráficos
-@st.experimental_memo(suppress_st_warning=True)
+@st.experimental_singleton(suppress_st_warning=True)
 def plot_charts(model, plots, package):
   for i in plots:
     try:
